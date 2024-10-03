60c6fe2b26b2a1f5e70a700c9c2a9f5a549a7af3
Gui-Angelo-Silva <guilherme_angelosilva@hotmail.com>
2024-10-03 09:18:53 -0300

    :sparkles: feat: PUT para atualizar veículo.

--- minimal-api/obj/Debug/net8.0/minimal-api.AssemblyInfo.cs
+++ minimal-api/obj/Debug/net8.0/minimal-api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("minimal-api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+45300d0b3672ec151495309dfed03fa407170e6f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+895b14c49e47309125d7476279a325cedd4cb626")]
 [assembly: System.Reflection.AssemblyProductAttribute("minimal-api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("minimal-api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
